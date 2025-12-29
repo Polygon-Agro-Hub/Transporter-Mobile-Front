dd2beaac00ce69471633eb5604b3f431ddd99530
Avishka777 <a.rathnakumara777@gmail.com>
2025-12-29 14:07:54 +0530

    Remove Unwanted Files and Fixed Minor Issues

--- environment/environment.ts
+++ environment/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMDU2MjExMC0xNjk4LTExZjAtOGY3Mi05MWZhM2QzZDhmODYiLCJzdWIiOiJTSE9VVE9VVF9BUElfVVNFUiIsImlhdCI6MTc0NDM1MDI1NCwiZXhwIjoyMDU5ODgzMDU0LCJzY29wZXMiOnsiYWN0aXZpdGllcyI6WyJyZWFkIiwid3JpdGUiXSwibWVzc2FnZXMiOlsicmVhZCIsIndyaXRlIl0sImNvbnRhY3RzIjpbInJlYWQiLCJ3cml0ZSJdfSwic29fdXNlcl9pZCI6IjgzOTkzIiwic29fdXNlcl9yb2xlIjoidXNlciIsInNvX3Byb2ZpbGUiOiJhbGwiLCJzb191c2VyX25hbWUiOiIiLCJzb19hcGlrZXkiOiJub25lIn0.cw29Ns80bCYCSuzgTX7qF1ZJzNteXi_hGD6MJ-tcoQg",
 
   // PROD --------------------
-  // API_BASE_URL: "https://transporter-mobile-api.vercel.app/transporter/",
+  // API_BASE_URL: "https://transporter-mobile-api-prod.vercel.app/transporter/",
   // SHOUTOUT_API_KEY:
   //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYjUxMWUzMC1mMjZjLTExZWYtOGQyZi1jNzhmOWJkMzU4NzciLCJzdWIiOiJTSE9VVE9VVF9BUElfVVNFUiIsImlhdCI6MTc0MDM3MzEzMywiZXhwIjoyMDU1OTA1OTMzLCJzY29wZXMiOnsiYWN0aXZpdGllcyI6WyJyZWFkIiwid3JpdGUiXSwibWVzc2FnZXMiOlsicmVhZCIsIndyaXRlIl0sImNvbnRhY3RzIjpbInJlYWQiLCJ3cml0ZSJdfSwic29fdXNlcl9pZCI6IjgzOTkzIiwic29fdXNlcl9yb2xlIjoidXNlciIsInNvX3Byb2ZpbGUiOiJhbGwiLCJzb191c2VyX25hbWUiOiIiLCJzb19hcGlrZXkiOiJub25lIn0.jqSNF1mJTmWHem1ZfjRQ1WKgsmB5N4tN8OOw7J9h6qI",
 
